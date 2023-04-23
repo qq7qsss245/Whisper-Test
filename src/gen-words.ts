0ddabbc95698c0cd21cf91adc5e4c9133515c0a2
qq7qsss245 <qq7qsss245@163.com>
2023-04-23 11:16:49 +0800

    fix gen words

--- src/gen-words.ts
+++ src/gen-words.ts
@@ -1,8 +1,8 @@
 //
-import { readFileSync, writeFileSync } from 'fs';
+import { readFileSync, writeFileSync, createWriteStream } from 'fs';
 import { join } from 'path';
-
-const gTTS = require('gtts');
+import axios from 'axios';
+const ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36';
 
 const loadWords = async () => {
     const words = readFileSync(join(__dirname, 'EnWords.csv')).toString();
@@ -10,16 +10,32 @@ const loadWords = async () => {
     const result = lines.map(line => {
         const word = line.split(',')[0].replace(/"/g, "");
         // return `https://ssl.gstatic.com/dictionary/static/sounds/oxford/${word}--_gb_1.mp3`
-        return `https://dict.youdao.com/dictvoice?type=0&audio=${word}`;
+        return {
+            word,
+            url: `https://dict.youdao.com/dictvoice?type=0&audio=${word}`
+        };
         // return word;
     });
-    // result.map(word => {
-    //     const gtts = new gTTS(word, 'en');
-    //     const filePath = join(__dirname, 'words', `${word}.mp3`)
-    //     gtts.save(filePath);
-    // })
-    const filePath = join(__dirname, 'words.txt')
-    writeFileSync(filePath, result.join(`\n`));
+    for (let item of result) {
+        try {
+            const filePath = join(__dirname, 'words', `${item.word}.mp3`);
+            const ws = createWriteStream(filePath);
+            const res = await axios.get(item.url, { responseType: 'stream', timeout: 50000, headers: { 'User-Agent': ua } });
+            res.data.pipe(ws);
+            console.log(`start downloading ${item.word}.mp3`);
+            await new Promise(resolve => {
+                res.data.on('close', () => {
+                    ws.close();
+                    console.log(`${item.word}.mp3 finished`);
+                    resolve(null);
+                })
+            })
+        }catch (e) {
+            continue;
+        }
+    }
+    // const filePath = join(__dirname, 'words.txt')
+    // writeFileSync(filePath, result.join(`\n`));
 };
 
 
