9f8f3cbdf310c8fd8f47b084eb1c03a23186abcd
qq7qsss245 <qq7qsss245@163.com>
2023-04-23 17:38:06 +0800

    fix

--- src/cpp.ts
+++ src/cpp.ts
@@ -20,7 +20,7 @@ const single = async  (word: string) => {
  const start = Date.now();
  const filePath = join(__dirname, 'words', `${word}.wav` );
  await new Promise(resolve => {
-  exec(`NV_GPU=1 ~/temp/whisper.cpp/main -m models/ggml-base.en.bin ${filePath} -of ${outputPath} --output-txt -l en`, resolve);
+  exec(`NV_GPU=1 ~/temp/whisper.cpp/main -m ~/temp/whisper.cpp/models/ggml-base.en.bin ${filePath} -of ${outputPath} --output-txt -l en`, resolve);
  });
  const end = Date.now();
  const s = moment.duration(end - start, "milliseconds").asSeconds();
