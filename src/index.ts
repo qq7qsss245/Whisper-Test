0c255f608a502c418a00e6ae42c2c1b9e39daba6
qq7qsss245 <qq7qsss245@163.com>
2023-04-23 15:16:20 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const runTest = async () => {
  const now = Date.now();
  for (let word of words) {
   const filePath = join(__dirname, 'words', `${word}.mp3` );
-  exec(`whisper ${filePath} --model ${model} --output_dir ${outputPath} --output_format txt`);
+  exec(`whisper ${filePath} --model ${model} --output_dir ${outputPath} --output_format txt --language en`);
   // console.log(word);
  }
  const seconds = moment.duration(Date.now() - now, "milliseconds").asSeconds();
